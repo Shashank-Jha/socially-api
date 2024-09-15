9bf09ecfe3cc31862388352dbe14ded14fdcfd5f
Shashank-Jha <jhashashank007@rediffmail.com>
2024-09-15 15:45:37 +0530

    init: socially api
    Signed-off-by: Shashank-Jha <jhashashank007@rediffmail.com>

---
+++ src/test/java/com/spyder/socially/SociallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spyder.socially;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
